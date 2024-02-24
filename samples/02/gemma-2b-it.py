af1ed56abc0e85c743d6310af84fe673b22b4828
Goto Hayato <gh640@hey.com>
2024-02-24 12:55:00 +0900

    Add sample 02

---
+++ samples/02/gemma-2b-it.py
@@ -0,0 +1,15 @@
+"""A sample to use Gemma 2B it.
+
+See: https://huggingface.co/google/gemma-2b-it
+"""
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
+model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it")
+
+input_text = "Write me a poem about Machine Learning."
+input_ids = tokenizer(input_text, return_tensors="pt")
+
+outputs = model.generate(**input_ids)
+print(tokenizer.decode(outputs[0]))
+
